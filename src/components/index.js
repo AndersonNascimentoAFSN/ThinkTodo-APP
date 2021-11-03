663f34e6ecc53ee046e851bda436e1721296d8b9
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-03 19:29:12 -0300

    feat: todo page - exporta componentes

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Header from './Header';
 import Title from './Title';
+import TaskTable from './TaskTable';
+import TaskForm from './TaskForm';
 
 export {
   Header,
   Title,
+  TaskTable,
+  TaskForm,
 };
